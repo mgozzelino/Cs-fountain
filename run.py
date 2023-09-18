9c12c9d4d0e42a1692b8a186c14f3b38c06d1034
mgozzelino <145259988+mgozzelino@users.noreply.github.com>
2023-09-18 11:45:09 +0200

    Update run.py

--- run.py
+++ run.py
@@ -8,13 +8,11 @@ Created on Thu Nov 18 11:33:25 2021
 
 import subprocess
 
-program_list = ['02_calc_y_Ts.py', '03_export_daily.py']
+program_list = ['00_merge_lock12', '01_collisional', '02_calc_y_Ts.py', '03_export_daily.py']
 
 for program in program_list:
     subprocess.call(['python', program])
     print("\n ...Finished:" + program + '\n')
     
     
-print('Aggiornato collisionale???????????')
-# print('Aggiornato incertezze???????????')
-print('Corretto clock PC vecchio?????????')
+print('Did you check initial collisional coefficient ???????????')
